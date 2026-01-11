3f0407900812d7ac3129c00349f791cd27b40473
Throy Towercamp <166675577+FDC-Genesis@users.noreply.github.com>
2026-01-11 21:30:13 +0800

    deploy

---
+++ vite.ts
@@ -0,0 +1,21 @@
+// don't import any modules that's related to vite to avoid deployment issues
+
+const inlineConfig = {
+  server: {
+    port: 5173,
+  },
+
+  build: {
+    outDir: "public/build",
+    emptyOutDir: true,
+    manifest: true,
+    rollupOptions: {
+      input: [
+        "resources/ts/app.ts"
+      ],
+    },
+  },
+  publicDir: "public/assets",
+};
+
+export default inlineConfig;
